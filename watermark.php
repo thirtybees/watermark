00a6822da7d5e4e41f8fddbf39c5343dec18f3fa
Petr Hučík <petr@getdatakick.com>
2023-07-11 11:02:48 +0200

    Release watermark v1.2.3

--- watermark.php
+++ watermark.php
@@ -41,7 +41,7 @@ class Watermark extends Module
     {
         $this->name = 'watermark';
         $this->tab = 'administration';
-        $this->version = '1.2.2';
+        $this->version = '1.2.3';
         $this->author = 'thirty bees';
         $this->need_instance = false;
         $this->tb_versions_compliancy = '>= 1.0.0';
